4df6dd059ef0b73352408f576c3b846c307ddd97
Patryk <patryk.szylin.dev@gmail.com>
2020-06-29 12:19:34 +0100

    testing fix for heroku deployment

--- AllTheBeans.API/Program.cs
+++ AllTheBeans.API/Program.cs
@@ -20,7 +20,8 @@ namespace AllTheBeans.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseKestrel();
                 }); 
     }
 }
